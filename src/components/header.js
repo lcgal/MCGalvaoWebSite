6322656245c8ab01e4689d65a9eab2d50922dda3
Lucas Galvão <lcollares@indracompany.com>
2020-08-16 03:59:56 -0300

    master innitial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,19 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from "./logo.png"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `lightgrey`,
+      marginBottom: `1.4rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        maxWidth: 1260,
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -24,12 +24,13 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          <img src={logo} alt="Logo" width="400px" />
         </Link>
       </h1>
     </div>
   </header>
 )
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
